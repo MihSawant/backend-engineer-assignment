7421914ef58027c222809144f39666c4dd43fe9b
Mihir Sawant <mih3190Sawant@outlook.com>
2022-05-19 11:53:00 +0530

    adding the main files

---
+++ src/main/java/com/nunam/backendengineerassignemnt/BackendEngineerAssignemntApplication.java
@@ -0,0 +1,13 @@
+package com.nunam.backendengineerassignemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineerAssignemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEngineerAssignemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nunam/backendengineerassignemnt/BackendEngineerAssignemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunam.backendengineerassignemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEngineerAssignemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
